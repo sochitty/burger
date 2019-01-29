84a1f760c15c73dc774cf7317195bc3c797c8fc6
chitty <rychitty@icloud.com>
2019-01-28 20:07:25 -0600

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
+        port: 8889,
         host: 'localhost',
         user: 'root',
         password: 'root',
