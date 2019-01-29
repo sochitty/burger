c32074cc981cd7ca2e58e81d46e4e142b227f080
chitty <rychitty@icloud.com>
2019-01-28 20:28:15 -0600

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,25 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        port: 8889,
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'burgers_db'
-    });
-};
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         port: 8889,
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'root',
+//         database: 'burgers_db'
+//     });
+// };
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
 
 // Make connection.
 connection.connect(function(err) {
